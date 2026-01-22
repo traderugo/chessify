8a37dde2e326e0bf4c75975801c46c110932c34e
Johnson Ugochukwu <86761845+traderugo@users.noreply.github.com>
2026-01-22 10:52:09 +0100

    major changes to the seach field ui

--- components/tournament/TournamentListItemCompact.js
+++ components/tournament/TournamentListItemCompact.js
@@ -32,6 +32,7 @@ export default function TournamentListItemCompact({
       href={`/tournaments/${tournament.id}`}
       className="
         group block
+        border border-gray-400
         bg-white dark:bg-[#0d1117]
         hover:bg-gray-50 dark:hover:bg-[#161b22]
         transition-colors
